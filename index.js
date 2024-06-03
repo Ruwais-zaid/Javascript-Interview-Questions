8f750b3420bc442fb9412fcc510fd5bf6676b521
Ruwais-zaid <zaid.btech2022sitpune.edu.in>
2024-06-03 22:20:46 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//Q 1
+console.log("Value of age is ",age)
+var age=20
+console.log("Value og age is",age)
+//Hoisted :- Javascript have Global excution Context
+//Q2 
+//age =100
+//console.log("Value of age is :" ,age)
+//let age=100
+//ans error because let is declared before use let have blocked scope
+
+//Q3
+myfun();
+var myfun = function(){
+    console.log("First")
+}
+myfun();
+function myfun(){
+    console.log("Second")
+}
+myfun();
